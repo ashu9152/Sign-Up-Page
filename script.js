a2b15f6546823bebe934026ba0fb53d543ab5dde
Ashutosh <omkarashu28@gmail.com>
2022-11-08 18:09:49 +0530

    Solved bug and completed project

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
-let form = document.getElementById('form')
-let username = document.getElementById('username')
-let email = document.getElementById('email')
-let password = document.getElementById('password')
-let password2 = document.getElementById('password2')
+const form = document.getElementById('form');
+const userName = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-if(form){
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
 
-    form.addEventListener('submit', ()=>{
-    let usernameValue = username.value.trim()
-    let emailValue = email.value.trim()
-    let passwordValue = password.value.trim()
-    let password2Value = password2.value.trim()
-    
-
-    if(usernameValue === ''){
-        showError(username,'User Name cannot be empty')
-       
-    }else{
-        success(username)
-       
-    }
-})
-    }else{
-        console.log('Error')
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = userName.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		onError(userName, 'Username cannot be blank');
+	} else {
+		onSuccess(userName);
+	}
+	
+	if(emailValue === '') {
+		onError(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		onError(email, 'Not a valid email');
+	} else {
+		onSuccess(email);
+	}
+	
+	if(passwordValue === '') {
+		onError(password, 'Password cannot be blank');
+	} else {
+		onSuccess(password);
+	}
+	
+	if(password2Value === '') {
+		onError(password2, 'Password2 is empty');
+	} else if(passwordValue !== password2Value) {
+		onError(password2, 'Passwords not same');
+	} else{
+		onSuccess(password2);
+	}
 }
 
-
-
-function showError(input, message) {
-    const formControl = input.parentElement  //.form-control
-    const small = formControl.querySelector('small')
-
-    //add error message inside small
-    small.innerText = message
-
-    //add error class
-    formControl.classname = 'form-control error'
+function onError(inputValue, message) {
+	const formControl = inputValue.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
 }
 
-function success (input){
-    const formControl = input.parentElement    //.form-control
-    formControl.classname = 'form-control success'
+function onSuccess(inputValue) {
+	const formControl = inputValue.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
